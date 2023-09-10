c9231a41bee0a39d76320c9fe9f1d874f55f2cf5
Scott Cowe <scott.t.cowe@gmail.com>
2023-09-10 21:48:29 +0100

    Rewrote and removed 2am code

--- tokenizer.py
+++ tokenizer.py
@@ -38,124 +38,41 @@ class Tokenizer:
     
     # Formats input so that a subtraction is an addition of a negative, also cleans up subtracting negatives and multiplying parenthesis
     def format_input(self):
-        output_string = ""
+        length = len(self.input)
 
-        for i in range(len(self.input)):
+        i = 0
+
+        while i < length:
             current_char = self.input[i]
-            previous_char = self.input[i - 1] if i != 0 else ""
+            previous_char = self.input[i - 1] if i > 0 else ""
 
-            if current_char == "(" and previous_char.isnumeric(): # multiplying brackets like 5(4 + 3)
-                output_string += "*" # so bracket is like 5*(4+3)
-            elif current_char.isnumeric() and previous_char == "-": 
-                output_string = output_string[:-1] # Removes last character (-) of output
-                output_string += "+-"
-            elif current_char == previous_char and current_char == "-":
-                output_string = output_string[:-1]
-                output_string += "+"
-                current_char = "" # I don't really like this solution but eh
+            if current_char == "(" and previous_char.isnumeric():
+                before = self.input[0:i]
+                after = self.input[i:]
+                self.input = before + "*" + after
+                length = len(self.input)
+                i += 2
+            elif self.isNumeric(current_char) and previous_char == "-":
+                if current_char == "-":
+                    before = self.input[0:i-1]
+                    after = self.input[i+1:]
+                    self.input = before + "+" + after
+                    i += 1
+                else:
+                    before = self.input[0:i-1]
+                    after = self.input[i-1:]
+                    self.input = before + "+" + after
+                    i += 2
+            else:
+                i += 1
 
-            # TODO: Fix -- 
-
-            output_string += current_char
-
-        self.input = output_string
         print(f"Formatted input is '{self.input}'")
 
     # Parses the input into an array of tokens
     def parse_input(self):
         output_list = []
 
-        input_string = self.input
-        input_string_length = len(self.input)
-
-        slicing = False
-        wasSlicing = False
-
-        i = 0
-
-        while i < input_string_length:
-            current_char = input_string[i]
-            previous_char = input_string[i - 1]
-
-            print(f"Input string: {input_string} {input_string_length}")
-            print(f"i: {i} {current_char}")
-
-            if self.isOperator(current_char):
-                output_list.append(Token(TokenType.OPERATOR, current_char))
-
-                # Slice
-                input_string = input_string[1:]
-                input_string_length = len(input_string)
-                i = -1
-                slicing = False
-            elif self.isNumeric(current_char):
-                slicing = True
-            else:
-                slicing = False
-                wasSlicing = True
-
-            if slicing == False and wasSlicing == True:
-                wasSlicing = False
-                slice = input_string[i:]
-                print(slice)
-
-            i += 1
-
-        """
-        start_index = 0
-        end_index = 0
-        
-        slicing = False
-
-        openingParentheisCount = 0
-        closingParenthesisCount = 0
-
-        for i in range(len(self.input)):
-            current_char = self.input[i]
-            toAppend = None
-            
-            if current_char == "(":
-                if not slicing:
-                    start_index = i
-                    end_index = i
-                    slicing = True
-                
-                end_index += 1
-                openingParentheisCount += 1
-            elif current_char == ")":
-                closingParenthesisCount += 1
-
-                if openingParentheisCount == closingParenthesisCount:
-                    print(f"end slice {i} {current_char}")
-                    print(f"{start_index}   {end_index}")
-                    openingParentheisCount = 0
-                    closingParenthesisCount = 0
-                else:
-                    print(f"skipped {i} {current_char}")
-                
-            elif self.isOperator(current_char):
-                toAppend = Token(TokenType.OPERATOR, current_char)
-                slicing = False
-            elif self.isNumeric(current_char):
-                if not slicing:
-                    start_index = i
-                    end_index = i
-
-                slicing = True
-                end_index += 1 
-
-            if i == len(self.input) - 1:
-                slicing = False
-                slicingParenthesis = False
-
-            if start_index != 0 and end_index != 0 and slicing == False:
-                slice = self.input[start_index:end_index]
-                output_list.append(Token(TokenType.NUMBER, slice))
-                start_index = 0
-                end_index = 0
-
-            if toAppend != None:
-                output_list.append(toAppend)"""
+        # Add this back
 
         print(f"Token values are: ")
         for i in range(len(output_list)):
